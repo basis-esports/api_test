8a6614d5e651c7bffd3454e74957869337bb5d9b
basisesports <72276743+basisesports@users.noreply.github.com>
2020-11-30 16:06:16 -0600

    Update riot.py

--- app/riot.py
+++ app/riot.py
@@ -1,21 +1,54 @@
-from app import app
+import requests
 from riotwatcher import LolWatcher, ApiError
 
-
-# global variables
-# api_key = 'RGAPI-xxxxx'
-# watcher = LolWatcher(api_key)
-# user_region = 'na1'
+RIOT_GAMES_API_KEY = os.environ.get('RIOT_GAMES_API_KEY')
 
 
-class Riot:
+class Summoner(db.Model):
+	__tablename__ = 'summoners'
+
+	id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+
+	summoner_name = db.Column(db.String(64), nullable=False)
+	summoner_region = db.Column(db.String(64), nullable=False)
+	current_tier = db.Column(db.String(64), nullable=False)
+	current_rank = db.Column(db.String(64), nullable=False)
+	summoner_points = db.Column(db.BigInteger, nullable=False)
+
     lol_watcher = LolWatcher(access_token=app.config['RIOT_GAMES_API_KEY'])
-    user_region = 'na1'
 
-    def get_summoner(self, summoner_id):
-        return self.lol_watcher.summoner.by_name(user_region, summoner_id)
+    # Relationships
 
-    def get_ranked_stats(self, summoner_id):
-        return self.lol_watcher.league.by_summoner(user_region, get_summoner(['id']))
+    def __init__(self, summoner_name, summoner_region, current_tier, current_rank, summoner_points):
+    	self.summoner_name = summoner_name
+    	self.summoner_region = summoner_region
+    	self.current_tier = current_tier
+    	self.current_rank = current_rank
+    	self.summoner_points = summoner_points
 
-riot = Riot()
+    def summoner_name():
+
+
+    def statgatherer():
+    	global element
+    	current_rank = (element['tier'] + ' ' + element['rank'])
+    	points = (element['leaguePoints'])
+
+
+
+# test
+lol_watcher = LolWatcher('RGAPI-96d87b95-6f83-4250-81b1-eec48daaa5a7')
+
+my_region = 'na1'
+
+me = lol_watcher.summoner.by_name(my_region, 'doublelift')
+
+my_ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
+
+#for element in my_ranked_stats:
+#	if element['queueType'] == 'RANKED_SOLO_5x5':
+#		statgatherer()
+#	else:
+#		print('Error - no data found - check server, and summoner name.')
+
+print(my_ranked_stats[0]['tier'])
