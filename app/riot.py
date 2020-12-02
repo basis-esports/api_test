f4376402bf122c93979607d769e9195cc53d74eb
basisesports <72276743+basisesports@users.noreply.github.com>
2020-12-02 15:15:36 -0600

    Update riot.py

--- app/riot.py
+++ app/riot.py
@@ -18,6 +18,12 @@ class Summoner(db.Model):
     lol_watcher = LolWatcher(access_token=app.config['RIOT_GAMES_API_KEY'])
 
     # Relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def update(self, raw):
+    	for field in ('summoner_name'):
+    		if field in raw:
+    			setattr(self, field, raw[field])
 
     def __init__(self, summoner_name, summoner_region, current_tier, current_rank, summoner_points):
     	self.summoner_name = summoner_name
@@ -26,14 +32,24 @@ class Summoner(db.Model):
     	self.current_rank = current_rank
     	self.summoner_points = summoner_points
 
-    def summoner_name():
+    # Launch stat tree - load server list, then load rank stats
+	def launchstattree(self,):
+		global element
+		from lib import apisettings
 
+		lol_watcher = LolWatcher(apisettings.yourapikey)
 
-    def statgatherer():
-    	global element
-    	current_rank = (element['tier'] + ' ' + element['rank'])
-    	points = (element['leaguePoints'])
+		serverselect()
 
+		me = lol_watcher.summoner.by_name(my_region, sumname)
+
+		my_ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
+
+		for element in my_ranked_stats:
+			if element['queueType'] == 'RANKED_SOLO_5x5':
+				gather_stats()
+		else:
+			return fail('Error - no data found - check server, and summoner name.')
 
 
 # test
@@ -45,10 +61,26 @@ me = lol_watcher.summoner.by_name(my_region, 'doublelift')
 
 my_ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
 
-#for element in my_ranked_stats:
-#	if element['queueType'] == 'RANKED_SOLO_5x5':
-#		statgatherer()
-#	else:
-#		print('Error - no data found - check server, and summoner name.')
-
 print(my_ranked_stats[0]['tier'])
+
+
+# Launches menu, using pick to allow the player to select what they want to do
+def summoner_name():
+	global sumname
+
+	sumname = input('Your summoner name: ')
+	sumname = sumname.translate(dict.fromkeys(map(ord, whitespace)))
+	launchstattree()
+
+
+# Gathers stats
+def gather_stats():
+	global element
+	current_tier = element['tier']
+	current_rank = element['rank']
+	points = (element['leaguePoints'])
+	wins = (element['wins'])
+	losses = (element['losses'])
+	total_games = wins + losses
+	rate = round(wins * 100 / total_games, 2)
+	summoner_name()
